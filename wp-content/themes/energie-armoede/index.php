d1393a17d08a58bd1ed4ea654aedc181a24969de
Andrea Vendrik <andrea.vendrik@gmail.com>
2016-11-18 15:55:01 +0100

    styled single page

--- wp-content/themes/energie-armoede/index.php
+++ wp-content/themes/energie-armoede/index.php
@@ -9,8 +9,8 @@ $recent_posts = wp_get_recent_posts( $args, ARRAY_A );
 
 		<?php if ( is_404() ) : ?>
 
-			<header class="page-header clr">
-				<h1 class="page-title"><?php esc_html_e( 'Oops! That page can&rsquo;t be found.', 'wpex' ); ?></h1>
+			<header class="page-header">
+				<h1 class="page-title"><?php esc_html_e( 'Oeps! Deze pagina bestaat niet.', 'wpex' ); ?></h1>
 			</header><!-- .page-header -->
 
 		<?php endif; ?>
--- wp-content/themes/energie-armoede/single.php
+++ wp-content/themes/energie-armoede/single.php
@@ -13,35 +13,64 @@ $related_posts = get_posts([
 
 $next_post = get_next_post($post->ID);
 $prev_post = get_adjacent_post($post->ID);
+
 ?>
 
-<header id="category-header">
-	<a href="<?php echo get_bloginfo('url'); ?>">terug naar overzicht</a>
-	<h2><?php echo $category->name; ?></h2>
+<header id="category-header" style="background-image:url('<?php echo z_taxonomy_image_url($category->term_id); ?>')" />
+	<h4><a href="<?php echo get_bloginfo('url'); ?>">< terug naar overzicht</a></h4>
+	<h2>Categorie: <?php echo $category->name; ?></h2>
 	<article><?php echo $category->description; ?></article>
-	<nav id="prev-next-post-nav">
-		<?php if($prev_post->ID): ?>
-			<a href="<?php echo get_permalink($prev_post->ID); ?>">Vorige post</a>
-		<?php endif; ?>
-		<?php if($next_post->ID): ?>
-			<a href="<?php echo get_permalink($next_post->ID); ?>">Volgende post</a>
-		<?php endif; ?>
-	</nav>
 </header>
 
+<nav id="prev-next-post-nav">
+	<div id="prev-post-nav">
+		<?php if($prev_post->ID): ?>
+			<a href="<?php echo get_permalink($prev_post->ID); ?>">
+				<svg width="15px" height="25px" viewBox="440 361 20 29" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+		    <polyline id="Page-1" stroke="#005A73" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" fill="none" points="442 388 456.018 376.203 442 364"></polyline>
+				</svg></a>
+		<?php endif; ?>
+	</div>
+	<div id="current-post-nav">
+		<h4>Artikel X/XX</h4>
+	</div>
+	<div id="next-post-nav">
+		<?php if($next_post->ID): ?>
+			<a href="<?php echo get_permalink($next_post->ID); ?>">
+				<svg width="15px" height="25px" viewBox="440 361 20 29" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+				<polyline id="Page-1" stroke="#005A73" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" fill="none" points="442 388 456.018 376.203 442 364"></polyline>
+			</a>
+		<?php endif; ?>
+	</div>
+</nav>
+
 <main id="content">
-	<h1><?php echo $post->post_title; ?></h1>
-	<article><p><?php echo $post->post_content; ?></p></article>
+
+	<article id="main-content">
+
+		<h1><?php echo $post->post_title; ?></h1>
+		<article>
+
+			<?php
+				$some_long_text =  $post->post_content;
+				echo wpautop( $some_long_text );
+				?>
+		</article>
+	</article>
+
+	<aside id="content-navigation">
+		<h1>Artikelen</h1>
+		<ul id="related-posts">
+			<?php foreach($related_posts as $related_post): ?>
+				<li>
+					<a href="<?php echo get_permalink($related_post->ID); ?>"><?php echo $related_post->post_title; ?></a>
+				</li>
+			<?php endforeach; ?>
+		</ul>
+	</aside>
+
 </main>
 
-<aside>
-	<ul id="related-posts">
-		<?php foreach($related_posts as $related_post): ?>
-			<li>
-				<a href="<?php echo get_permalink($related_post->ID); ?>"><?php echo $related_post->post_title; ?></a>
-			</li>
-		<?php endforeach; ?>
-	</ul>
-</aside>
+
 
 <?php get_footer(); ?>
