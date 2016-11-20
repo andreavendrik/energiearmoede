7e169a3acd549cdbf4f7b80214100fc7e5e96a0c
Andrea Vendrik <andrea.vendrik@gmail.com>
2016-11-20 11:07:32 +0100

    swapped category article list and main content on single page

--- wp-content/themes/energie-armoede/single.php
+++ wp-content/themes/energie-armoede/single.php
@@ -8,8 +8,14 @@ $category = $categories[0];
 $related_posts = get_posts([
 	"category_name" => $category->name,
 	"offset" => 0,
-	"posts_per_page" => -1
+	"posts_per_page" => -1,
+	"order" => "ASC"
 ]);
+
+$post_no = 0;
+foreach($related_posts as $key => $related_post):
+if($related_post->ID == $post->ID) $post_no = ($key+1);
+endforeach;
 
 $next_post = get_next_post($post->ID);
 $prev_post = get_adjacent_post($post->ID);
@@ -19,45 +25,14 @@ $prev_post = get_adjacent_post($post->ID);
 <header id="category-header" class="general-header" style="background-image:url('<?php echo z_taxonomy_image_url($category->term_id); ?>')" />
 	<h4><a href="<?php echo get_bloginfo('url'); ?>">< terug naar overzicht</a></h4>
 	<h2>Categorie: <?php echo $category->name; ?></h2>
-	<article><?php echo $category->description; ?></article>
 </header>
-
-<nav id="prev-next-post-nav">
-	<div id="prev-post-nav">
-		<?php if($prev_post->ID): ?>
-			<a href="<?php echo get_permalink($prev_post->ID); ?>">
-				<svg width="15px" height="25px" viewBox="440 361 20 29" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-		    <polyline id="Page-1" stroke="#005A73" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" fill="none" points="442 388 456.018 376.203 442 364"></polyline>
-				</svg></a>
-		<?php endif; ?>
-	</div>
-	<div id="current-post-nav">
-		<h4>Artikel X/XX</h4>
-	</div>
-	<div id="next-post-nav">
-		<?php if($next_post->ID): ?>
-			<a href="<?php echo get_permalink($next_post->ID); ?>">
-				<svg width="15px" height="25px" viewBox="440 361 20 29" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-				<polyline id="Page-1" stroke="#005A73" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" fill="none" points="442 388 456.018 376.203 442 364"></polyline>
-			</a>
-		<?php endif; ?>
-	</div>
-</nav>
 
 <main class="general-content" id="category-content">
 
-	<article>
-		<h1><?php echo $post->post_title; ?></h1>
-		<div>
-			<?php
-				$auto_paragraphs =  $post->post_content;
-				echo wpautop( $auto_paragraphs );
-				?>
-		</div>
-	</article>
+<!-- List of articles in same category -->
 
 	<aside id="content-navigation">
-		<h1>Artikel lijst</h1>
+		<div><?php echo $category->description; ?></div>
 		<ul id="related-posts">
 			<?php foreach($related_posts as $related_post): ?>
 				<li>
@@ -66,6 +41,50 @@ $prev_post = get_adjacent_post($post->ID);
 			<?php endforeach; ?>
 		</ul>
 	</aside>
+
+
+	<!-- Previous post button -->
+
+	<article>
+
+		<nav id="prev-next-post-nav">
+			<div id="prev-post-nav">
+				<?php if($prev_post->ID): ?>
+					<a href="<?php echo get_permalink($prev_post->ID); ?>">
+						<svg width="15px" height="25px" viewBox="440 361 20 29" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+				    <polyline id="Page-1" stroke="#005A73" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" fill="none" points="442 388 456.018 376.203 442 364"></polyline>
+						</svg></a>
+				<?php endif; ?>
+			</div>
+
+			<!-- Current post display -->
+			<div id="current-post-nav">
+				<h4>Artikel <?php echo $post_no; ?>/<?php echo count($related_posts); ?></h4>
+			</div>
+
+			<!-- Next post button -->
+
+			<div id="next-post-nav">
+				<?php if($next_post->ID): ?>
+					<a href="<?php echo get_permalink($next_post->ID); ?>">
+						<svg width="15px" height="25px" viewBox="440 361 20 29" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
+						<polyline id="Page-1" stroke="#005A73" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" fill="none" points="442 388 456.018 376.203 442 364"></polyline>
+					</a>
+				<?php endif; ?>
+			</div>
+		</nav>
+
+		<!-- Content of article -->
+
+
+		<h1><?php echo $post->post_title; ?></h1>
+		<div>
+			<?php
+				$auto_paragraphs =  $post->post_content;
+				echo wpautop( $auto_paragraphs );
+				?>
+		</div>
+	</article>
 
 </main>
 
